00842a32b7723e3f1000030e1aa1f13af275b251
mrwizard2 <4625443+mrwizard2@users.noreply.github.com>
2022-09-03 18:58:26 -0500

    1 file modified

--- client/GetPricesUniswap.js
+++ client/GetPricesUniswap.js
@@ -11,7 +11,7 @@ const provider = new ethers.providers.JsonRpcProvider(
   'https://eth-mainnet.g.alchemy.com/v2/oR2RI3ISaPeRDfEm4IB-f5ios943aPoy'
 );
 
-async function getPrice() {
+async function getPrice(addressFrom, addressTo, amountInHuman) {
   const quoterAddress = '0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6';
 
   const quoterContract = new ethers.Contract(
@@ -20,7 +20,28 @@ async function getPrice() {
     provider
   );
 
-  console.log(quoterContract);
+  const amountIn = ethers.utils.parseUnits(amountInHuman, 6);
+  const quoteAmountOut = await quoterContract.callStatic.quoteExactInputSingle(
+    addressFrom,
+    addressTo,
+    3000,
+    amountIn.toString(),
+    0
+  );
+
+  // Output the amount
+  const amount = ethers.utils.formatUnits(quoteAmountOut.toString(), 18);
+
+  return amount;
 }
 
-getPrice();
+const main = async () => {
+  const addressFrom = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'; // USDC
+  const addressTo = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'; // WETH
+  const amountInHuman = '1560';
+
+  const amountOut = await getPrice(addressFrom, addressTo, amountInHuman);
+  console.log(amountOut);
+};
+
+main();
